1b7cf7b59d477113b6115bb3b4bcb90a9e1f6e39
Krzysiek <kkijas@jaktestowac.pl>
2023-11-02 19:04:51 +0100

    fix: tests config url

--- tests/helpers/helpers.js
+++ tests/helpers/helpers.js
@@ -12,12 +12,11 @@ function gracefulQuit() {
 async function setupEnv() {
   const restoreResponse = await request.get("/api/restoreDB");
   expect(restoreResponse.status).to.equal(201);
-
   // Lower log level to WARNING:
   const requestBody = {
     currentLogLevel: logLevel,
   };
-  const response = await request.post("/api/config").send(requestBody);
+  const response = await request.post("/api/config/all").send(requestBody);
   expect(response.status).to.equal(200);
 }
 
