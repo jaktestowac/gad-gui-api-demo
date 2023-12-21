67be2938cd5bd198ca6f1fd3e6e38fe6ac7693d7
Krzysiek <kkijas@jaktestowac.pl>
2023-12-21 16:33:49 +0100

    feat: feature flags on UI

--- public/tools/index.js
+++ public/tools/index.js
@@ -21,3 +21,52 @@ const pathToSchema = `${window.location.origin}${window.location.pathname}schema
 if (swaggerElement) {
   swaggerElement.href = `https://editor.swagger.io/?url=${pathToSchema.replace("index.html", "")}`;
 }
+
+fetch("/api/config/features")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const tableDiv = document.getElementById("featureTable");
+    tableDiv.innerHTML = "";
+    const table = document.createElement("table");
+
+    for (const feature of Object.keys(data.config)) {
+      const row = document.createElement("tr");
+
+      const labelCell = document.createElement("td");
+      const labelText = document.createTextNode(feature);
+      labelCell.appendChild(labelText);
+      row.appendChild(labelCell);
+
+      const checkboxCell = document.createElement("td");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.checked = data.config[feature];
+      checkbox.addEventListener("change", function () {
+        handleCheckboxChange(feature, checkbox.checked);
+      });
+
+      checkboxCell.appendChild(checkbox);
+      row.appendChild(checkboxCell);
+
+      table.appendChild(row);
+    }
+    tableDiv.appendChild(table);
+  });
+
+function handleCheckboxChange(feature, isChecked) {
+  console.log(`Feature ${feature} was ${isChecked ? "checked" : "unchecked"}`);
+  const payload = {};
+  payload[feature] = isChecked;
+  fetch("/api/config/features", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  }).then((response) => {
+    console.log(`Response for feature ${feature}: ${response.status}`);
+    return response.json();
+  });
+}
