80c8d5c0b2b6be0b2e6eca4cd3c040c8f4abf54b
Krzysiek <kkijas@jaktestowac.pl>
2024-01-08 23:56:13 +0100

    feat: bugs enable / disable from UI

--- public/tools/index.js
+++ public/tools/index.js
@@ -45,7 +45,7 @@ fetch("/api/config/features")
       checkbox.checked = data.config[feature];
       checkbox.id = `checkbox-${feature}`;
       checkbox.addEventListener("change", function () {
-        handleCheckboxChange(feature, checkbox.checked);
+        handleFeatureCheckboxChange(feature, checkbox.checked);
       });
 
       checkboxCell.appendChild(checkbox);
@@ -56,7 +56,7 @@ fetch("/api/config/features")
     tableDiv.appendChild(table);
   });
 
-function handleCheckboxChange(feature, isChecked) {
+function handleFeatureCheckboxChange(feature, isChecked) {
   console.log(`Feature ${feature} was ${isChecked ? "checked" : "unchecked"}`);
   const payload = {};
   payload[feature] = isChecked;
@@ -68,6 +68,56 @@ function handleCheckboxChange(feature, isChecked) {
     body: JSON.stringify(payload),
   }).then((response) => {
     console.log(`Response for feature ${feature}: ${response.status}`);
+    return response.json();
+  });
+}
+
+fetch("/api/config/bugs")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const tableDiv = document.getElementById("bugsTable");
+    tableDiv.innerHTML = "";
+    const table = document.createElement("table");
+
+    for (const bug of Object.keys(data.config)) {
+      const row = document.createElement("tr");
+
+      const labelCell = document.createElement("td");
+      const labelText = document.createTextNode(bug);
+      labelCell.appendChild(labelText);
+      row.appendChild(labelCell);
+
+      const checkboxCell = document.createElement("td");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.checked = data.config[bug];
+      checkbox.id = `checkbox-${bug}`;
+      checkbox.addEventListener("change", function () {
+        handleBugCheckboxChange(bug, checkbox.checked);
+      });
+
+      checkboxCell.appendChild(checkbox);
+      row.appendChild(checkboxCell);
+
+      table.appendChild(row);
+    }
+    tableDiv.appendChild(table);
+  });
+
+function handleBugCheckboxChange(bug, isChecked) {
+  console.log(`Bug ${bug} was ${isChecked ? "checked" : "unchecked"}`);
+  const payload = {};
+  payload[bug] = isChecked;
+  fetch("/api/config/bugs", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  }).then((response) => {
+    console.log(`Response for bug ${bug}: ${response.status}`);
     return response.json();
   });
 }
