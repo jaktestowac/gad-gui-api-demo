4f0567e88fae84463a9145295150999b7b2ad95e
Krzysiek <kkijas@jaktestowac.pl>
2024-02-22 22:42:24 +0100

    chore: bump version

--- public/version.js
+++ public/version.js
@@ -1,6 +1,6 @@
 const versionElement = document.querySelector("#version");
 
-const appVersion = "v2.5.4";
+const appVersion = "v2.5.5";
 
 if (versionElement) {
   versionElement.innerHTML = appVersion;
