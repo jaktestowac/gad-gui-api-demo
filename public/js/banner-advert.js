f8e206a5e03507704fef18cdebf0ac46b75b9c4d
Krzysiek <kkijas@jaktestowac.pl>
2024-03-28 23:52:24 +0100

    feat: random banners

--- public/js/banner-advert.js
+++ public/js/banner-advert.js
@@ -1,11 +1,11 @@
-const advertArticlesBanner = `<div class="popup-overlay">
-<div class="popup-container">
+const advertArticlesBanner1 = `<div class="popup-overlay">
+<div class="popup-container" style="background-image: url('../images/advert1-bg.jpg'); ">
 
   <div class="right">
     <div class="skip-button">Skip in 5s</div>
 
     <p class="logo">🦎GAD</p>
-    <h2 class="popup-heading">Deliver Quality</h2>
+    <h2 class="popup-heading">Gain Knowledge</h2>
     <p class="description">
       Wide range of articles on various quality related topics. Click the button
       below to view more details.
@@ -15,6 +15,63 @@ const advertArticlesBanner = `<div class="popup-overlay">
   </div>
 </div>
 </div>`;
+
+const advertSudokuBanner = `<div class="popup-overlay">
+<div class="popup-container" style="background-image: url('../images/advert2-bg.jpg'); ">
+
+  <div class="right2">
+    <div class="skip-button">Skip in 5s</div>
+
+    <p class="logo">🦎GAD</p>
+    <h2 class="popup-heading">SUDOKU</h2>
+    <p class="description">
+      Play Sudoku 
+      <br>and train your brain🧠!
+      <br>Improve your logical thinking skills. Have fun🎉
+    </p>
+
+    <a href="./sudoku.html" class="visit-button">Play Sudoku</a>
+  </div>
+</div>
+</div>`;
+
+const advertCommentsBanner = `<div class="popup-overlay">
+<div class="popup-container" style="background-image: url('../images/advert3-bg.jpg'); ">
+
+  <div class="right">
+    <div class="skip-button">Skip in 5s</div>
+
+    <p class="logo">🦎GAD</p>
+    <h2 class="popup-heading">Exchange Thoughts</h2>
+    <p class="description">
+      Share your thoughts and experience with other readers. Click the button
+      below to view the comments.
+    </p>
+
+    <a href="./comments.html" class="visit-button">Visit Comments</a>
+  </div>
+</div>
+</div>`;
+
+const advertStatisticsBanner = `<div class="popup-overlay">
+<div class="popup-container" style="background-image: url('../images/advert5-bg.jpg'); ">
+
+  <div class="right">
+    <div class="skip-button">Skip in 5s</div>
+
+    <p class="logo">🦎GAD</p>
+    <h2 class="popup-heading">World of Charts</h2>
+    <p class="description"> 
+      Check out the statistics of the articles and comments. Click the button
+      below to view the statistics.
+    </p>
+
+    <a href="./stats.html" class="visit-button">Visit Statistics</a>
+  </div>
+</div>
+</div>`;
+
+const listOfBanners = [advertArticlesBanner1, advertCommentsBanner, advertStatisticsBanner, advertSudokuBanner];
 
 let remainingTime = 5;
 let allowedToSkip = false;
@@ -73,11 +130,15 @@ const skipAd = () => {
   saveAcceptInCookies(0.005);
 };
 
+function getRandomBanner() {
+  return listOfBanners[Math.floor(Math.random() * listOfBanners.length)];
+}
+
 function displayAd() {
   if (wasAccepted() === true) {
     return;
   }
-  document.body.insertAdjacentHTML("beforeend", advertArticlesBanner);
+  document.body.insertAdjacentHTML("beforeend", getRandomBanner());
   const skipButton = document.querySelector(".popup-container .skip-button");
   showAd();
   skipButton.addEventListener("click", () => {
