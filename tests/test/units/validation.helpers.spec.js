b0e80b435d10e9b45f991837cd3759ad4d98a0bd
Krzysiek <kkijas@jaktestowac.pl>
2024-06-02 10:39:13 +0200

    chore: improve tests

--- tests/test/units/validation.helpers.spec.js
+++ tests/test/units/validation.helpers.spec.js
@@ -9,19 +9,19 @@ describe("validation helpers", async () => {
 
   describe("isDateInFuture", async () => {
     [
-      ["2020-12-12T12:12:12Z", false],
-      [new Date().toISOString(), false],
-      [getCurrentDate(), false],
-      [addOffsetToDateString(new Date().toISOString(), "-11:00"), false],
-      [addOffsetToDateString(new Date().toISOString(), "-04:00"), false],
-      [addOffsetToDateString(new Date().toISOString(), "-02:00"), false],
-      [addOffsetToDateString(new Date().toISOString(), "+02:00"), false],
-      [addOffsetToDateString(new Date().toISOString(), "+04:00"), false],
-      [addOffsetToDateString(new Date().toISOString(), "+11:00"), false],
-      [getCurrentDate(0, 0, 12), true],
-      [getCurrentDate(1, 0, 0), true],
-      [addOffsetToDateString(getCurrentDate(1, 0, 0), "-02:00"), true],
-      ["2040-12-12T12:12:12Z", true],
+      ["2020-12-12T12:12:12Z", false, -60],
+      [new Date().toISOString(), false, -120],
+      [getCurrentDate(), false, -120],
+      [addOffsetToDateString(new Date().toISOString(), "-11:00"), false, -660],
+      [addOffsetToDateString(new Date().toISOString(), "-04:00"), false, -240],
+      [addOffsetToDateString(new Date().toISOString(), "-02:00"), false, -120],
+      [addOffsetToDateString(new Date().toISOString(), "+02:00"), false, 120],
+      [addOffsetToDateString(new Date().toISOString(), "+04:00"), false, 240],
+      [addOffsetToDateString(new Date().toISOString(), "+11:00"), false, 660],
+      [getCurrentDate(0, 0, 12), true, -120],
+      [getCurrentDate(1, 0, 0), true, -120],
+      [addOffsetToDateString(getCurrentDate(1, 0, 0), "-02:00", -120), true],
+      ["2040-12-12T12:12:12Z", true, -60],
     ].forEach((inputDateStringPairs) => {
       it(`check date - ${inputDateStringPairs[0]} - in future - ${inputDateStringPairs[1]}`, async () => {
         // Arrange:
@@ -32,6 +32,11 @@ describe("validation helpers", async () => {
 
         // Assert:
         expect(result.isDateInFuture, JSON.stringify(result, null, 2)).to.eql(inputDateStringPairs[1]);
+        if (inputDateStringPairs[2] !== undefined) {
+          expect(result.inputDateTimezoneOffsetInMinutes, JSON.stringify(result, null, 2)).to.eql(
+            inputDateStringPairs[2]
+          );
+        }
       });
     });
   });
