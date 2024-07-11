8cb0c5dafdb32138b3f43ffad0e5706276385c10
Krzysiek <kkijas@jaktestowac.pl>
2024-07-11 10:15:20 +0200

    bump: gad version

--- public/version.js
+++ public/version.js
@@ -1,6 +1,6 @@
 const versionElement = document.querySelector("#version");
 
-const appVersion = "v2.7.1";
+const appVersion = "v2.7.2";
 
 if (versionElement) {
   versionElement.innerHTML = appVersion;
