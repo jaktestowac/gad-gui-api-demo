30d428e3578f27ff6aa25d92b611604f84afc207
Krzysiek <kkijas@jaktestowac.pl>
2024-10-09 16:11:13 +0200

    chore: bum version to 2.7.6

--- public/version.js
+++ public/version.js
@@ -1,6 +1,6 @@
 const versionElement = document.querySelector("#version");
 
-const appVersion = "v2.7.5";
+const appVersion = "v2.7.6";
 
 if (versionElement) {
   versionElement.innerHTML = appVersion;
