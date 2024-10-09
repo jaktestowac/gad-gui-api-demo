5f16670f1e9238657b2d87546e2ff9d4996308b3
Krzysiek <kkijas@jaktestowac.pl>
2024-10-09 16:11:41 +0200

    revert: surname validation #45

--- public/js/register.js
+++ public/js/register.js
@@ -114,8 +114,8 @@ document.querySelector("#registerForm").addEventListener("submit", function (e) 
   let formVal = new octaValidate("registerForm");
   formVal.customRule("DATE", /^\d{4}-\d{2}-\d{2}$/, "Date must be in format YYYY-MM-DD");
   // const surnameRegexp = /^[A-Z]{1}[a-zA-Z-]{0,}[a-z]{1}$/;
-  const surnameRegexp = /^[a-zA-Z-]{0,}$/;
-  formVal.customRule("SURNAME", surnameRegexp, "Please enter only letter or hyphen. Must start with capital letter");
+  const surnameRegexp = /^[A-Z]{1}[a-zA-Z\s]{1,}$/;
+  formVal.customRule("SURNAME", surnameRegexp, "Please enter only letter. Must start with capital letter");
 
   e.preventDefault();
   //invoke the validate method
