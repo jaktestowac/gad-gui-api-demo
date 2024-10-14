b31cd9262e42f9fe02ba67d5aec0ee1e8c180cde
Freudenberger <Freudenberger@users.noreply.github.com>
2024-10-14 16:41:02 +0200

    feat: practice pages with errors in console (#49)

--- public/version.js
+++ public/version.js
@@ -1,6 +1,6 @@
 const versionElement = document.querySelector("#version");
 
-const appVersion = "v2.7.6";
+const appVersion = "v2.7.7";
 
 if (versionElement) {
   versionElement.innerHTML = appVersion;
