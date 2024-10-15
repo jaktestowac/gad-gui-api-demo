147c81dc0be4b9ce6c41c8064bf42f017877d33f
Freudenberger <Freudenberger@users.noreply.github.com>
2024-10-15 06:56:35 +0200

    bv2.7.8 (#50)
    * fix: style in backoffice
    
    * chore: bump version to 2.7.8

--- public/version.js
+++ public/version.js
@@ -1,6 +1,6 @@
 const versionElement = document.querySelector("#version");
 
-const appVersion = "v2.7.7";
+const appVersion = "v2.7.8";
 
 if (versionElement) {
   versionElement.innerHTML = appVersion;
