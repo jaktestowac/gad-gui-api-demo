20ecbc3180d507a6f18ca380d885284a5557322b
Krzysiek <kkijas@jaktestowac.pl>
2025-02-08 22:45:42 +0100

    fix: websockets for glitch

--- public/js/wsh.js
+++ public/js/wsh.js
@@ -5,5 +5,9 @@ if (addrParts[1] !== undefined && !isNaN(addrParts[1])) {
   addrParts[1] = (parseInt(addrParts[1]) + 10).toString();
 }
 
-const wsAddr = addrParts.join(":");
+// check if http or https
+const protocol = window.location.protocol;
+const wsProtocol = protocol === "https:" ? "wss" : "ws";
+
+const wsAddr = `${wsProtocol}://${addrParts.join(":")}`;
 console.log("wsAddr", wsAddr);
