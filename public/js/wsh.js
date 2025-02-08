784c333afc6344ee18e99d338f2b90e4b1b61925
Krzysiek <kkijas@jaktestowac.pl>
2025-02-08 22:36:05 +0100

    fix: websockets for glitch

--- public/js/wsh.js
+++ public/js/wsh.js
@@ -1,5 +1,9 @@
 const addr = window.location.host;
 const addrParts = addr.split(":");
-addrParts[1] = (parseInt(addrParts[1]) + 10).toString();
+
+if (addrParts[1] !== undefined && !isNaN(addrParts[1])) {
+  addrParts[1] = (parseInt(addrParts[1]) + 10).toString();
+}
+
 const wsAddr = addrParts.join(":");
 console.log("wsAddr", wsAddr);
