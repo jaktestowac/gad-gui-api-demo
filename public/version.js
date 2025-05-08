ccdb2b2e399cdf9145383dacabe6599e98604d12
Krzysiek <kkijas@jaktestowac.pl>
2025-05-09 00:44:56 +0200

    chore: update version to v2.8.3

--- public/version.js
+++ public/version.js
@@ -1,6 +1,6 @@
 const versionElement = document.querySelector("#version");
 
-const appVersion = "v2.8.2";
+const appVersion = "v2.8.3";
 
 if (versionElement) {
   versionElement.innerHTML = appVersion;
