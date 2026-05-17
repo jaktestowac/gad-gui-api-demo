263f115eca3e7f79962af6065956ce0c9f0c3b40
Krzysiek <kkijas@jaktestowac.pl>
2026-05-17 18:00:58 +0200

    chore(release): update version to 2.9.0 in all relevant files

--- public/version.js
+++ public/version.js
@@ -1,6 +1,6 @@
 const versionElement = document.querySelector("#version");
 
-const appVersion = "v2.8.9";
+const appVersion = "v2.9.0";
 
 if (versionElement) {
   versionElement.innerHTML = appVersion;
